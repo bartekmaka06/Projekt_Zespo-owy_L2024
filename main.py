71c912613a7e844ffc28526643ecdc4bf6b548ef
Pbad <okmalko4@gmail.com>
2024-04-02 23:10:00 +0200

    Opracowano pomysl na backend

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from weather import Weather
 pogoda = Weather("Warszawa")
 print(pogoda.status)
 print(pogoda.json)
+
+
+#example of usage
+print(pogoda.day[1].temperature)
--- weather.py
+++ weather.py
@@ -2,6 +2,7 @@ import requests
 import json
 class Weather:
   def __init__(self, city):
+    self.day = []
     self.json = None
     self.city = city
     self.status = self.get_weather()
@@ -11,7 +12,25 @@ class Weather:
     url = f'https://pro.openweathermap.org/data/2.5/forecast/climate?q={self.city}&appid={API_KEY}'
     res = requests.get(url)
     self.json = res.json()
+    #tutaj dodac petle ktora utworzy tabele dni i przypisze im pogody
+    #self.days.append(Day("15"))
     return res
+
+
+
+class Day:
+  def __init__(self,temperature_avg,temperature_min,temperature_max,overall_icon,pressure,humidity,rain,sunrise,
+               sunset,wind_speed):
+    self.temperature_avg = temperature_avg
+    self.temperature_min = temperature_min
+    self.temperature_max = temperature_max
+    self.overall_icon = overall_icon
+    self.pressure = pressure
+    self.humidity = humidity
+    self.rain = rain
+    self.sunrise = sunrise
+    self.sunset = sunset
+    self.wind_speed = wind_speed
 
 
 
